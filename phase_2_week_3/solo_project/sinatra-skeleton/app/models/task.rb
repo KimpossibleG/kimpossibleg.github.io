2e4549fe96127f9a7ff3bafe8b1189d3955bedd0
Kim Girard <kimberley.girard@gmail.com>
2014-06-20 11:43:04 -0500

    joined tables

--- phase_2_week_3/solo_project/sinatra-skeleton/app/models/task.rb
+++ phase_2_week_3/solo_project/sinatra-skeleton/app/models/task.rb
@@ -2,5 +2,5 @@ class Task < ActiveRecord::Base
 	validates :content, presence: true
 	validates :complete, :inclusion => {:in => [true, false]}
 
-	
+	belongs_to :user
 end
--- phase_2_week_3/solo_project/sinatra-skeleton/app/models/user.rb
+++ phase_2_week_3/solo_project/sinatra-skeleton/app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ActiveRecord::Base
 	validates :name, presence: true, uniqueness: true
 
+	has_many :tasks
+
 	include BCrypt
 
     def password
